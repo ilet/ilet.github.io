a5df9b27571c052b1b6e63ca5d45d903fd646af5
Ilet Chuu <35316871+ilet@users.noreply.github.com>
2018-01-17 01:03:06 +0100

    Update typ-example.js

--- typ-example.js
+++ typ-example.js
@@ -1,5 +1,5 @@
 function json_decode(str){
   if (type.tm("s", str))
     return new Function("return" + str)(); // bcaus eval is for babies
-  else throw new TypeError("str isn't a String dude");
+  else throw new TypeError(str+" isn't a String dude");
 }
