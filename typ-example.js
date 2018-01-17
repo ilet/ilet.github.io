bd3a7b1f6994d34057ea9125ec985e037abd62b0
Ilet Chuu <35316871+ilet@users.noreply.github.com>
2018-01-17 01:06:52 +0100

    Update typ-example.js

--- typ-example.js
+++ typ-example.js
@@ -3,3 +3,14 @@ function json_decode(str){
     return new Function("return" + str)(); // bcaus eval is for babies
   else throw new TypeError(str+" isn't a String dude");
 }
+
+function int_pow(x, y){
+  var r=x;
+  if (type.tm("n n", x, y))
+    for (;y-->0;)
+      r*=x;
+  else throw new TypeError(x+ " or " + y+" isn't a number, dude");
+  return r;
+}
+int_pow(2, 3); // 16
+int_pow(2, 3); // some error
