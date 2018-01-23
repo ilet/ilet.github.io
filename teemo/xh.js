4ca7480af82ebcbdb57fb50ad2cace569fe77ace
Ilet Chuu <35316871+ilet@users.noreply.github.com>
2018-01-23 22:06:34 +0100

    Update xh.js

--- teemo/xh.js
+++ teemo/xh.js
@@ -1,4 +1,4 @@
 {
-	name: "bh1", regEx: "^!test$",
-	act: dmsg => dmsg.reply("R.I.P Xana's Dreams of Hope.")
+	name: "dummyText", regEx: "^!dummy$",
+	act: dmsg => dmsg.reply("Kavij's this long dummy text is pretty longer than usual sentence or it may look like a theory with no particular topic or it does make sense to read the text without getting confused because of this goddamned sentence which is actually long enough.")
 }
