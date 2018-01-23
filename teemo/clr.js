0deca4f42ee2cc4c0e6f677e5e7ddbaa12d0f675
Ilet Chuu <35316871+ilet@users.noreply.github.com>
2018-01-23 18:34:44 +0100

    Update clr.js

--- teemo/clr.js
+++ teemo/clr.js
@@ -3,10 +3,10 @@
 	regEx: "^!clear messages$",
 	act: dmsg => {
 		if (dmsg.member.hasPermission("MANAGE_MESSAGES"))
-            dmsg.channel.fetchMessages().then(function(L){
+			dmsg.channel.fetchMessages().then(function(L){
 				dmsg.channel.bulkDelete(L);
 			}, function(err){
 				dmsg.channel.send("ERROR: ERROR CLEARING CHANNEL.")
-			});                       
+		});                       
 	}
 }
