033053c1d442868460f925a30854241be580ffa0
Ilet Chuu <35316871+ilet@users.noreply.github.com>
2018-01-23 22:09:25 +0100

    Update xh.js

--- teemo/xh.js
+++ teemo/xh.js
@@ -1,4 +1,6 @@
 {
-	name: "dummyText", regEx: "^!dummy$",
-	act: dmsg => dmsg.reply("Kavij's this long dummy text is pretty longer than usual sentence or it may look like a theory with no particular topic or it does make sense to read the text without getting confused because of this goddamned sentence which is actually long enough.")
+	name: "dummyText",
+	regEx: "^!dummy$",
+	example: "!dummy",
+	act: dmsg => dmsg.reply("Kavij's long dummy text is pretty longer than usual sentence(array of words) or it may look like a theory with no particular topic or it does make sense to read the text without getting confused because of this goddamned sentence which is actually long enough.")
 }
