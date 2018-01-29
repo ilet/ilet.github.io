36b3e87b3dea0e73297f19548676bd773fb53b8b
Ilet Chuu <35316871+ilet@users.noreply.github.com>
2018-01-29 17:37:24 +0100

    Delete xh.js

--- teemo/xh.js
+++
@@ -1,6 +0,0 @@
-{
-	name: "dummyText",
-	regEx: "^!dummy$",
-	example: "!dummy",
-	act: dmsg => dmsg.reply("Kavij's long dummy text is pretty longer than usual sentence(array of words) or it may look like a theory with no particular topic or it does make sense to read the text without getting confused because of this goddamned sentence which is actually long enough.")
-}
