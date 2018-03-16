cb7e529c4af388f876de6037028cc748ec7c2acf
Ilet Chuu <35316871+ilet@users.noreply.github.com>
2018-03-16 13:24:25 +0100

    Update dispatch-tumblr.js

--- tc/dispatch-tumblr.js
+++ tc/dispatch-tumblr.js
@@ -1,12 +1,12 @@
 ~function(){
-  var ctitle = document.title;
-  var t,ico = $("link[rel*=icon][href]"), h = "", b = "";
-  if (ico.length) ico = ico[0].outerHTML;
-  if ($("[data-dhnd]").length)
-  {
-    h+=(t=$("[data-dhnd][data-head]")).length?t.attr("data-head"):"";
-    b+=(t=$("[data-dhnd][data-body]")).length?t.attr("data-body"):"";
-  }
-  document.body.parentNode.innerHTML="<html><head>"+h+ico+"</head><body>"+b+"</body></html>";
-  document.title = ctitle;
+	var _ = document, T = _.title, I = $("link[rel*=icon][href]"), D = $("<html><head></head><body></body></html>");
+	if (I.length) I = I[0].outerHTML;
+	if ($("[data-dhnd]").length)
+	{
+		var d = $("[data-dhnd]");
+		$("head", D).append($("data-head", d));
+		$("body", D).append($("data-body", d));
+	}
+	_.body.parentNode.innerHTML = D[0].outerHTML;
+	_.title = T;
 }();
