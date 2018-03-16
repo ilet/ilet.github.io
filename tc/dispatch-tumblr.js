4b0ff8fd2a08a5b059f9c842b87b91eb1ad9bd9d
Ilet Chuu <35316871+ilet@users.noreply.github.com>
2018-03-16 13:36:19 +0100

    Update dispatch-tumblr.js

--- tc/dispatch-tumblr.js
+++ tc/dispatch-tumblr.js
@@ -1,15 +1,11 @@
 ~function(){
-	var _ = document, T = _.title, I = $("link[rel*=icon][href]"), D = $("<i>");
+	var d, _ = document, T = _.title, I = $("link[rel*=icon][href]"), D = $("<i>");
 	if (I.length) I = I[0].outerHTML;
-	if ($("[data-dhnd]").length)
-	{
-		var d = $("[data-dhnd]");
-		$(D).append("<head>"+d.attr("data-head")+"</head>");
-		$(D).append("<body>"+d.attr("data-body")+"</body>");
-		console.log(0, d.attr("data-head"));
-		console.log(1, d.attr("data-body"));
-		console.log(2, D);
-	}
+	if ((d = $("[data-dhnd]")).length)
+		$(D).append(
+			"<head>"+d.attr("data-head")+"</head>"+
+			"<body>"+d.attr("data-body")+"</body>"
+		);
 	_.body.parentNode.innerHTML = "<html>"+D.html()+"</html>";
 	_.title = T;
 }();
