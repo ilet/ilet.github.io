e9fdd59cc21f60f9753f7fd8940ad4ff28359297
Ilet Chuu <35316871+ilet@users.noreply.github.com>
2018-03-16 13:10:16 +0100

    Update dispatch-tumblr.js

--- tc/dispatch-tumblr.js
+++ tc/dispatch-tumblr.js
@@ -1,9 +1,12 @@
 ~function(){
   var ctitle = document.title;
-  var ico = $("link[rel*=icon][href]"), h = "<b>Please be patient, it may take upto several centuries.</b>";
+  var t,ico = $("link[rel*=icon][href]"), h = "", b = "";
   if (ico.length) ico = ico[0].outerHTML;
   if ($("[data-dhnd]").length)
-    h+=$("[data-dhnd][data-mobile=yes]").length?'<meta name="viewport" content="width=device-width">',"";
-  document.body.parentNode.innerHTML="<html><head>"+h+ico+"</head><body></body></html>";
+  {
+    h+=(t=$("[data-dhnd][data-head]")).length?t.attr("data-head"):"";
+    b+=(t=$("[data-dhnd][data-body]")).length?t.attr("data-body"):"";
+  }
+  document.body.parentNode.innerHTML="<html><head>"+h+ico+"</head><body>"+b+"</body></html>";
   document.title = ctitle;
 }();
